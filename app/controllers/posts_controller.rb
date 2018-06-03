986648d443bf641bf0ff3836fd21bf957f960864
Ben <ben.challenor@btopenworld.com>
2018-06-03 19:43:28 +0100

    Add create and show pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,29 @@
 class PostsController < ApplicationController
-  def index
-  end
+  def index; end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+      # if saved go to show posts
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    # find the post by id(created by default) saved to an instant variable
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+    # require post table, save title and body attributes
   end
 end
