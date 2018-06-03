7ab0379ac9f4a57e56ddfcb8f4237036d5d5cea2
Ben <ben.challenor@btopenworld.com>
2018-06-03 20:59:58 +0100

    Add edit page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,7 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.all.order('created_at DESC')
-    # go to all posts and show them in descending order
+     # go to all posts and show them in descending order
    end
 
   def new
@@ -22,6 +21,19 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     # find the post by id(created by default) saved to an instant variable
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   private
